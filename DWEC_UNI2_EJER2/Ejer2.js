57368b79b641742d7e371c6998a58e68aa76ed8a
Alejandromc45 <alejandromc45@educastur.es>
2023-10-02 13:12:02 +0200

    Ejer2

---
+++ DWEC_UNI2_EJER2/Ejer2.js
@@ -0,0 +1,19 @@
+// let nombre = "Alejandro";
+// var apellido="Menendez";
+// let altura = 1.74;
+// let pie=42;
+
+var nombre =prompt("Nombre");
+var apellido=prompt("Apellidos");
+var altura= parseInt(prompt("Altura"));
+var pie=parseInt(prompt("Pie")); 
+
+console.log(typeof nombre + " " + typeof apellido + " " + typeof altura+ " " + typeof pie)
+
+
+console.log(nombre +"\n"+apellido)
+
+console.log(altura + pie)
+
+
+
